03295e8db79b797e5c54b6745164fcb65e375ea7
Cipher-in-progress <148627760+Cipher-in-progress@users.noreply.github.com>
2023-10-24 14:17:56 +0530

    Create posts.js

---
+++ posts.js
@@ -0,0 +1,20 @@
+// posts.js
+
+const posts = [
+    {
+        id: 1,
+        title: "First Post",
+        content: "This is the content of the first post.",
+        date: "2023-10-24"
+    },
+    {
+        id: 2,
+        title: "Second Post",
+        content: "This is the content of the second post.",
+        date: "2023-10-25"
+    },
+    // Add more post objects as needed
+];
+
+// Export the posts array so it can be used in other JavaScript files
+export default posts;
